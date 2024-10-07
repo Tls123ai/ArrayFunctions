8b8586d39eca236d9b0434c5f1af5f99687619a3
Tls123ai <harryragot@gmail.com>
2024-10-07 08:45:50 +0800

    Update Task1.py

---
+++ Task1.py
@@ -0,0 +1,8 @@
+#Task1
+num = int(input("How many numbers would you like to add? "))
+numbers = []
+for i in range(num):
+    Number = int(input("Enter the number that you would like to add: "))
+    numbers.append(Number)
+    
+print(numbers)
